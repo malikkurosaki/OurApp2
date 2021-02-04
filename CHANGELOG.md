d4be15e1edc50fa2f329fa96d5d249e410a6ff3a
malikkurosaki <kurosakiblackangel@gmail.com>
2021-02-04 22:04:08 +0800

    add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [0.0.1] - 2021-01-23.
 
 * create your own calendar
+
+## [0.0.2] - 2021-01-23.
+
+* fix bugs
---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_json/calendar_json.dart';
+
+main() {
+  runApp(Coba());
+}
+
+class Coba extends StatelessWidget {
+  final day = <String>['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+  final calen = Calendar.json(DateTime.now().toString());
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: GridView.count(
+                    crossAxisCount: 7,
+                    children: [
+                      for(var cl in calen['perMonth'])
+                      Container(
+                        child: Center(
+                          child: Text(cl['date'].toString(),
+                            style: TextStyle(
+                              color: cl['type'] == 1?Colors.black: Colors.grey[300]
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
